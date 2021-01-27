f0d678415ec4da7773c330b3b32e36ca9fca75da
Q3-113 <simonoswald@outlook.com>
2021-01-27 18:55:41 +0100

    get entries from Toggl API and print in EOD format

---
+++ autoEOD.py
@@ -0,0 +1,42 @@
+import requests 
+from datetime import datetime
+
+workspace_id = "" # Toggl workspace id
+api_token = "" # Toggl API token
+user = "" # Toggl user name
+
+base_url = "https://api.track.toggl.com/reports/api/v2/details"
+user_agent = "simonoswald@outlook.com"
+date = datetime.today().strftime('%Y-%m-%d')
+
+auth = (api_token, "api_token")
+
+payload = {
+    "user_agent": user_agent,
+    "workspace_id": workspace_id,
+    "since": date
+}
+
+r = requests.get(base_url, auth=auth, params=payload).json()
+
+events = r["data"]
+projects = set()
+events_by_project = {}
+
+for e in events:
+    p = e["project"]
+    event = e["description"]
+
+    if not e["user"] == user:
+        continue
+
+    projects.add(p)
+
+    if not p in events_by_project:
+        events_by_project[p] = [event]
+    else:
+        events_by_project[p].insert(0, event)
+    
+for p in projects:
+    print(p + "\nEOD: " + ", ".join(e for e in events_by_project[p]) + "\n")
+
